77a8e3fd3b3e85c0d2c1f9309e455a95e5e4746c
chaudarybilal <chaudarybilal618@gmail.com>
2023-10-26 13:08:07 +0500

    write media query

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// toggel
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+
+
+
+// scroll section
 let sections=document.querySelectorAll('section');
 let navlinks=document.querySelectorAll('header nav a');
 window.onscroll=()=>{
@@ -18,5 +31,20 @@ window.onscroll=()=>{
 
     let header=document.querySelector('header');
     header.classList.toggle('sticky',window.scrollY > 100);
-    
+// remove toggel
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 };
+// scroll revel
+
+ScrollReveal({ 
+    // reset: true ,
+    distance:"80px",
+    duration:2000,
+    delay:200
+});
+ScrollReveal().reveal('.home-content, .heading', { orignal:top});
+ScrollReveal().reveal('.home-img, .skill-container,.project-box, .contact form ', { orignal:"bottom"});
+ScrollReveal().reveal('.home-content h1, .about-img', { orignal:"left"});
+ScrollReveal().reveal('.home-content p, .about-content', { orignal:"right"});
